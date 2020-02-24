ade142beb619a32a4c41aa9bb46e235e70775806
pramodkhapare <pramod.khapare@enwidth.com>
2020-02-24 15:09:03 +0530

    Program to perform different set operations like in mathematics

--- set_operations.py
+++ set_operations.py
@@ -1,8 +1,8 @@
 # Program to perform different set operations like in mathematics
 
 # define three sets
-E = {0, 2, 4, 6, 8};
-N = {1, 2, 3, 4, 5};
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
 
 # set union
 print("Union of E and N is",E | N)
