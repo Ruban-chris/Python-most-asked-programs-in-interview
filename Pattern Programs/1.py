8fda34fe3df9f463f0a2e8aed9eed700fce0a37d
pramodkhapare <pramod.khapare@enwidth.com>
2020-03-02 11:03:34 +0530

    Print a number pattern using a for loop and range function

---
+++ Pattern Programs/1.py
@@ -0,0 +1,20 @@
+# Print Number pattern in Python
+
+# 10 is the total number to print
+for num in range(10):
+    for i in range(num):
+        print (num, end=" ") #print number
+    # new line after each row to display pattern correctly
+    print("\n")
+
+# Sample Output 
+
+# 1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5 
+# 6 6 6 6 6 6 
+# 7 7 7 7 7 7 7 
+# 8 8 8 8 8 8 8 8 
+# 9 9 9 9 9 9 9 9 9
